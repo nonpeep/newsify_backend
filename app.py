b034fcdd3c55e96bfce17f497fcf0ac4d82dc1a9
thennal <thennal10@gmail.com>
2022-02-25 23:47:45 +0530

    fuck you python imports

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-import scraper
+import re
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, request
 from transformers import pipeline
 
@@ -9,7 +11,27 @@ model = pipeline('question-answering', model='./model', tokenizer='./model')
  
 app = Flask(__name__)
 
- 
+
+def gnews_get(url):
+    list_of_urls = [] 
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    article_div = soup.find_all('a')
+    for a in article_div:
+        if 'google' not in str(a):
+            link = re.findall('https[\S]+' ,a['href'])
+        if a.text != '':
+            try:
+                end_index = link[0].find('&')
+                link = link[0][0:end_index]
+                list_of_urls.append((a.find('div').text, link))
+            except:
+                continue
+    
+    nextpage = soup.find(attrs={"aria-label": "Next page"})['href']
+    return list_of_urls, f'https://www.google.com{nextpage}'
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     return model(request.json)
@@ -21,7 +43,7 @@ def headlines():
         url = f'https://www.google.com/search?q={site}&tbm=nws&ei'
     else:
         url = request.json['url']
-    headlines, nextp = scraper.gnews_get(url)
+    headlines, nextp = gnews_get(url)
     return {'headlines': headlines, 'nextpage': nextp}
 
 if __name__ == "__main__":
