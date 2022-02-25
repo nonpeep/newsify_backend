6f7a2dfdbe94ebb981a48e5469f4521a1572a3f4
thennal <thennal10@gmail.com>
2022-02-25 16:17:25 +0530

    Add gnews scraper and headlines endpoint Credits to Ganesh for the scraper

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
-import json
- 
+import scraper
 from flask import Flask, request
- 
-from transformers import  pipeline
+from transformers import pipeline
+
 
 model_name = "deepset/roberta-base-squad2"
 model = pipeline('question-answering', model='./model', tokenizer='./model')
@@ -13,7 +12,16 @@ app = Flask(__name__)
  
 @app.route('/predict', methods=['POST'])
 def predict():
-   return model(request.json)
-  
+    return model(request.json)
+
+@app.route('/headlines', methods=['POST'])
+def headlines():
+    if site := request.json.get('site'):
+        url = f'https://www.google.com/search?q={site}&tbm=nws&ei'
+    else:
+        url = request.json['url']
+    headlines, nextp = scraper.gnews_get(url)
+    return {'headlines': headlines, 'nextpage': nextp}
+
 if __name__ == "__main__":
    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
